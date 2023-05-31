4f772f15e9d0baef301fe1d4e0bb83d5020db36a
RomanAndrusiak <romko164@gmail.com>
2023-05-31 09:28:29 +0300

    initialize project

---
+++ src/test/java/sonechko/project/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sonechko.project.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
